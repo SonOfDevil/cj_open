8793422425cf9716a6bfe3c1e19cc26dd1a799c0
looky3 <sameness3@gmail.com>
2019-11-19 19:07:31 +0900

    story page - slide 추가

--- project/js/common.js
+++ project/js/common.js
@@ -1,71 +1,177 @@
 // Move to Top screen
-  var mybutton = document.querySelector('.top-btn');
+var mybutton = document.querySelector('.top-btn');
 
-  // When the user scrolls down 20px from the top of the document, show the button
-  window.onscroll = function () {
-    scrollFunction()
-  };
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction()
+};
 
-  function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      mybutton.style.display = "block";
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
 
-    } else {
-      mybutton.style.display = "none";
-    }
+  } else {
+    mybutton.style.display = "none";
   }
+}
 
-  // When the user clicks on the button, scroll to the top of the document
-  function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 
 
-;(function () {
-  var imgSrc = $('.room-img').attr('src');
+;
+(function () {
+
+  // Image change
   var roomList = $('.room-list');
   var lists = roomList.children('li');
-  var imgbox = roomList.siblings('.room-img');
-
-  var imgUrl = imgbox.attr('src');
-  var splitUrl = imgUrl.split('/');
   var imgPath = './images/space/';
 
-
-
-  var arSplitUrl = sOriginImgUrl.split("/");    //   "/" 로 전체 url 을 나눈다
-  var nArLength = arSplitUrl.length;
-  var arFileName = arSplitUrl[nArLength - 1];   // 나누어진 배열의 맨 끝이 파일명이다
-  var arSplitFileName = sImgUrl.split(".");   // 파일명을 다시 "." 로 나누면 파일이름과 확장자로 나뉜다
-  var sFileName = arSplitFileName[0];         // 파일이름
-  var sFileExtension = arSplitFileName[1]     // 확장자
-
-
-
-
-
-
   lists.hover(function () {
+    var imgGroup = $(this).data('index');
+    var imgNum = 0 + String($(this).index() + 1);
+
     $(this).siblings('li').removeClass('active');
     $(this).toggleClass('active');
-    console.log($(this).index());
-
-
-    imgbox.attr('src', imgPath + 'asdf' +'.png');
-    // var imgSrc = $('.room-img').attr("src");
-
-    // $('.room-img').attr('src').replace('img-room01.png', 'img-room02.png')
+    $(this).parent().siblings('.room-img').attr('src', imgPath + 'img-room' + imgGroup + imgNum + '.png');
   });
 
-
+  // tab toggle
   var tab = $('.open-space-tab li');
-  tab.on('click', function(){
+  tab.on('click', function () {
     $(this).siblings().removeClass('active');
     $(this).toggleClass('active');
     $('.open-introdution-wrap').toggleClass('active');
   })
+
+
+
+  // Story page - Slider
+  $(".slider").slick({
+    // dots: true,
+    // infinite: true,
+    speed: 20,
+    autoplaySpeed: 2000,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    autoplay: true,
+    draggable: false,
+    arrows: false,
+    variableWidth: true,
+    centerPadding: '20px',
+    // responsive: [
+    //   {
+    //     breakpoint: 768,
+    //     settings: {
+    //     arrows: false,
+    //     centerMode: true,
+    //     centerPadding: '40px',
+    //     slidesToShow: 3
+    //     }
+    //     },
+    //     {
+    //     breakpoint: 480,
+    //     settings: {
+    //     arrows: false,
+    //     centerMode: true,
+    //     centerPadding: '40px',
+    //     slidesToShow: 1
+    //     }
+    //   }
+    // ]
+
+    // responsive: [{
+    //   breakpoint: 600,
+    //   settings: {
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1
+    //   }
+    // },
+    // {
+    //    breakpoint: 400,
+    //    settings: {
+    //       arrows: false,
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1
+    //    }
+    // }]
+  });
+  $('.slider').on('afterChange', function (event, slick, currentSlide, nextSlide) {
+    var count = currentSlide / 2;
+
+    // var count = slickNext + 1;
+    // count === 0 ? count = 5 : count = count / 2;
+
+    console.log(count);
+    $('.story-tab li').siblings('li').removeClass('active');
+    $('.story-tab li').eq(count).toggleClass('active');
+    // count++;
+
+
+  });
+
+
+  // Slider button Event
+  var moveTabStory = function () {
+    var currentSlideNum = $('.slider.story').slick('slickCurrentSlide');
+    var tabIndex = currentSlideNum / 2;
+
+    $('.story-tab li').siblings('li').removeClass('active');
+    $('.story-tab li').eq(tabIndex).toggleClass('active');
+
+    $('.sub-head.story li').removeClass('active');
+    $('.sub-head.story li').eq(tabIndex).toggleClass('active');
+
+  }
+  var moveTabMusic = function () {
+    var currentSlideNum = $('.slider.music').slick('slickCurrentSlide');
+    var tabIndex = currentSlideNum / 2;
+    $('.music-tab li').siblings('li').removeClass('active');
+    $('.music-tab li').eq(tabIndex).toggleClass('active');
+
+    $('.sub-head.music li').removeClass('active');
+    $('.sub-head.music li').eq(tabIndex).toggleClass('active');
+  }
+
+  $('.slider-arrow.story .prev-arrow').click(function () {
+    $('.slider.story').slick('slickPrev');
+    moveTabStory()
+  })
+  $('.slider-arrow.story .next-arrow').click(function () {
+    $('.slider.story').slick('slickNext');
+    moveTabStory()
+
+  })
+  $('.slider-arrow.music .prev-arrow').click(function () {
+    $('.slider.music').slick('slickPrev');
+    moveTabMusic()
+  })
+  $('.slider-arrow.music .next-arrow').click(function () {
+    $('.slider.music').slick('slickNext');
+    moveTabMusic()
+  })
+
+  // Slider Tab Event
+  $('.story-tab li, .music-tab li').on('click', function () {
+    $(this).siblings('li').removeClass('active');
+    $(this).toggleClass('active');
+
+    // tab 클릭시 slide 변경
+    var slideNo = $(this).index() * 2;
+    $(".slider").slick('slickGoTo', slideNo);
+
+    // tab 클릭시 제목 변경
+    $(this).parents().siblings('.sub-head').children('li').removeClass('active');
+    $(this).parents().siblings('.sub-head').children('li').eq($(this).index()).toggleClass('active');
+
+  });
+
+
+
+
+
 })();
-
-
